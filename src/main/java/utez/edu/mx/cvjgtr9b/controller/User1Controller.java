c2b7d02f9cfe35bcc3bb398efd2d4935234734b8
NONAMEB-312 <20223tn114@utez.edu.mx>
2025-05-27 16:17:00 -0600

    no c

--- src/main/java/utez/edu/mx/cvjgtr9b/controller/User1Controller.java
+++ src/main/java/utez/edu/mx/cvjgtr9b/controller/User1Controller.java
@@ -1,5 +1,10 @@
 package utez.edu.mx.cvjgtr9b.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping("/api/user1")
 @CrossOrigin("*")
