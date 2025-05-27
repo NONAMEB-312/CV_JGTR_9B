65926ce8238fe947d7f4a08bdcf8e04dd8194cc7
NONAMEB-312 <20223tn114@utez.edu.mx>
2025-05-27 15:50:17 -0600

    [

---
+++ src/main/java/utez/edu/mx/cvjgtr9b/Cvjgtr9bApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cvjgtr9b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cvjgtr9bApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cvjgtr9bApplication.class, args);
+    }
+
+}
---
+++ src/test/java/utez/edu/mx/cvjgtr9b/Cvjgtr9bApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cvjgtr9b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cvjgtr9bApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
